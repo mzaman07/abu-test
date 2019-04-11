908d6fd3295cffb06124d95d6a983fe06f90d8ce
mzaman07 <9009585+mzaman07@users.noreply.github.com>
2019-04-11 03:44:56 -0600

    setting up angular material for site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+// routing
 import { AppRoutingModule } from './app-routing.module';
+
+// components
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CluesByDateComponent } from './clues-by-date/clues-by-date.component';
+import { CluesByRandomComponent } from './clues-by-random/clues-by-random.component';
+import { CluesByCategoryComponent } from './clues-by-category/clues-by-category.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CategoriesComponent } from './categories/categories.component';
+import { SearchCategoryComponent } from './search-category/search-category.component';
+
+
+// material modules
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatSelectModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+} from '@angular/material';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CluesByDateComponent,
+    CluesByRandomComponent,
+    CluesByCategoryComponent,
+    CategoriesComponent,
+    SearchCategoryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
